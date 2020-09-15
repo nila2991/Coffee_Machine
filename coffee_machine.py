076b3acd91f7189d3ce35a4b8357efeef888c99a
Olga Nilova <nila2991@gmail.com>
2020-09-15 15:29:30 +0200

    Final

--- coffee_machine.py
+++ coffee_machine.py
@@ -4,6 +4,25 @@ milk = 540
 coffee_beans = 120
 cups = 9
 
+class Coffee_Machine():
+  state = 'None'
+
+  def decision(self, choice):
+    if self.state == 'Start':
+      return all_action(choice)
+    if self.state == 'buy_coffee':
+      buy_coffee(choice)
+    if self.state == 'fill_machine':
+      return choice
+
+
+def check(name, resource, cup_need):
+  if cup_need > resource:
+    return name + ', '
+  else:
+    return '+'
+
+
 def machine_state():
   print('The coffee machine has:')
   print(water, 'of water')
@@ -12,27 +31,15 @@ def machine_state():
   print(cups, 'of disposable cups')
   print('${}'.format(money), 'of money')
 
-def check(name, resource, cup_need):
-  if cup_need > resource:
-    return name + ', '
-  else:
-    return '+'
-
-def make_coffee(water_cup, milk_cup, beans_cup, money_cup):
-  global water, milk, coffee_beans, cups, money
-  water -= water_cup
-  milk -= milk_cup
-  coffee_beans -= beans_cup
-  cups -= 1
-  money += money_cup
-
-
 def espresso():
   global water, coffee_beans, cups, money
   enough = check('water', water, 250) + check('coffee beans', coffee_beans, 16) + check('disposable cups', cups, 1)
   if enough == '+++':
-    make_coffee(250, 0, 16, 4)
     print('I have enough resources, making you a coffee!')
+    water -= 250
+    coffee_beans -= 16
+    cups -= 1
+    money += 4
   else:
     print('Sorry, not enough {}!'.format(enough.strip(' ,+')))
 
@@ -41,8 +48,12 @@ def latte():
   global water, milk, coffee_beans, cups, money
   enough = check('water', water, 350) + check('milk', milk, 75) + check('coffee beans', coffee_beans, 20) + check('disposable cups', cups, 1)
   if enough == '++++':
-    make_coffee(350, 75, 20, 7)
     print('I have enough resources, making you a coffee!')
+    water -= 350
+    milk -= 75
+    coffee_beans -= 20
+    cups -= 1
+    money += 7
   else:
     print('Sorry, not enough {}!'.format(enough.strip(' ,+')))
 
@@ -50,14 +61,16 @@ def cappuccino():
   global water, milk, coffee_beans, cups, money
   enough = check('water', water, 200) + check('milk', milk, 100) + check('coffee beans', coffee_beans, 12) + check('disposable cups', cups, 1)
   if enough == '++++':
-    make_coffee(200, 100, 12, 6)
     print('I have enough resources, making you a coffee!')
+    water -= 200
+    milk -= 100
+    coffee_beans -= 12
+    cups -= 1
+    money += 6
   else:
     print('Sorry, not enough {}!'.format(enough.strip(' ,+')))
 
-def buy_coffee():
-  print('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:')
-  clients_coffee = input()
+def buy_coffee(clients_coffee):
   if clients_coffee == '1':
     espresso()
   elif clients_coffee == '2':
@@ -70,13 +83,13 @@ def buy_coffee():
 def fill_machine():
   global water, milk, coffee_beans, cups
   print('Write how many ml of water do you want to add:')
-  water += int(input())
+  water += new_client.decision(int(input()))
   print('Write how many ml of milk do you want to add:')
-  milk += int(input())
+  milk += new_client.decision(int(input()))
   print('Write how many grams of coffee beans do you want to add:')
-  coffee_beans += int(input())
+  coffee_beans += new_client.decision(int(input()))
   print('Write how many disposable cups of coffee do you want to add:')
-  cups += int(input())
+  cups += new_client.decision(int(input()))
 
 def take_money():
   global money
@@ -84,9 +97,13 @@ def take_money():
   money = 0
 
 def all_action(action):
+  print()
   if action == 'buy':
-    buy_coffee()
+    new_client.state = 'buy_coffee'
+    print('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:')
+    new_client.decision(input())
   elif action == 'fill':
+    new_client.state = 'fill_machine'
     fill_machine()
   elif action == 'take':
     take_money()
@@ -97,11 +114,11 @@ def all_action(action):
   print()
 
 def input_inf():
+  new_client.state = 'Start'
   print('Write action (buy, fill, take, remaining, exit):')
-  action = input()
-  print()
-  return all_action(action)
+  return new_client.decision(input())
+
+new_client = Coffee_Machine()
 
 while input_inf() != 0:
   continue
-
